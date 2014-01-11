8feaa33003f4a9c52325715f4aa2515e52bdf14f
tylerdurden2010 <tylerdurdn2010@gmail.com>
2014-01-12 01:21:32 +0800

    Scanner 80 port
    scann

---
+++ ThWget.py
@@ -0,0 +1,18 @@
+import threading,os,time
+def GetSome(IP):
+    command = "wget "+IP+" --wait=0.7 --max-redirect=0 --user-agent=\"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)\" -O index.html."+IP+""
+    f = os.system(command)
+    
+
+IPHandle = open("IP","r")
+test = IPHandle.readline()
+test = test.strip('\n')
+while test:
+    for i in range(5):
+        t = threading.Thread(target=GetSome,args=(test,))
+        t.start()
+        test = IPHandle.readline()
+        test = test.strip('\n')
+    time.sleep(5)
+    test = IPHandle.readline()
+    test = test.strip('\n')
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ is_alive_ping()
   ping -c 1 $1 > /dev/null
   [ $? -eq 0 ] && echo $i
 }
-for i in 10.1.1.{1..254} 
+for i in 10.125.11.{1..254} 
 do
 is_alive_ping $i & disown
 done
